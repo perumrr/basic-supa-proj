96d110f20f11776ad3c9022674db05e8f3857007
perumrr <74992461+perumrr@users.noreply.github.com>
2025-03-15 20:01:53 +0000

    new link

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-import { createClient } from 'https://cdn.skypack.dev/@supabase/supabase-js@2.7.1'
+import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://ccbvauyhuwfvrotpfoxd.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNjYnZhdXlodXdmdnJvdHBmb3hkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDIwNjYwMDcsImV4cCI6MjA1NzY0MjAwN30.M3b7bxl_jjAkWBChiXlFgYuVXX0oYwrl52cKWvStKpo'
